1bd3593b80bfe0060600a060b091d28f53ea2373
unknown <suryu7240@gmail.com>
2021-11-19 02:51:53 +0530

    added sorted option

--- script.js
+++ script.js
@@ -4,39 +4,50 @@ const data=[
         score:5,
         ll1:true,
         ll2:true,
-        ll3:true
+        ll3:true,
+        ll:3
     },
     {
         name:"Me",
         score:95,
         ll1:true,
         ll2:true,
-        ll3:true
+        ll3:true,
+        ll:3
     },
     {
         name:"Shit",
         score:24,
         ll1:true,
         ll2:true,
-        ll3:true
+        ll3:true,
+        ll:3
     },
     {
         name:"ALI",
         score:50,
         ll1:true,
         ll2:true,
-        ll3:true
+        ll3:true,
+        ll:3
     },
     {
         name:"Ranuk",
         score:15,
         ll1:true,
         ll2:true,
-        ll3:true
+        ll3:true,
+        ll:3
     }
-]
+];
+data.sort((a, b)=>{
+    if(a.score - b.score)
+        return  b.score-a.score;
+    else
+        return a.ll-b.ll;});
 const genrateTable=(data)=>{
     const table=document.querySelector(".tables");
+    // data.sort((a,b)=>(a.price>b.price));
     data.forEach((item,idx)=>{
         const li=document.createElement("li");
         li.className="base-row";
@@ -51,12 +62,17 @@ const genrateTable=(data)=>{
         div1.innerHTML=idx+1;
         div3.innerHTML=item.name;
         div2.innerHTML=item.score;
+        if(item.ll1)
+            div4.innerHTML+="<img src='ll1.png' alt='lifeline-1'></img>";
+        if(item.ll2)
+            div4.innerHTML+="<img src='ll2.png' alt='lifeline-2'></img>";
+        if(item.ll3)
+            div4.innerHTML+="<img src='ll3.png' alt='lifeline-3'></img>";
         li.appendChild(div1);
         li.appendChild(div3);
         li.appendChild(div2);
         li.appendChild(div4);
         table.appendChild(li);
-        console.log(item.idx);
     });
 };
 genrateTable(data);
